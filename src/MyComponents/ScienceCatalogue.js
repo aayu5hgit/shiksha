2935ee665c1bacc16cfb8a3d350cfe5edeab1e76
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-09-16 00:30:03 +0530

    (add): hindi cards

--- src/MyComponents/ScienceCatalogue.js
+++ src/MyComponents/ScienceCatalogue.js
@@ -1,79 +1,75 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import {Link,NavLink} from "react-router-dom"
-import vol from './../images/volume.png' 
-import profile from './../images/profile.png' 
+import { Link } from "react-router-dom";
+import vol from "./../images/volume.png";
+import profile from "./../images/profile.png";
 import { useSpeechSynthesis } from "react-speech-kit";
 
 function ScienceCatalogue() {
-  const { speak } = useSpeechSynthesis()
-  const evs = 'Environmental Science-1. It contains 12 lectures and 7 quizes'
-  const evs2 = 'Environmental Science-2. It contains 12 lectures and 7 quizes'
-  const evsprac = 'Environmental Science-Practical. It contains the practice of basic science experiments'
+  const { speak } = useSpeechSynthesis();
+  const evs = "Environmental Science-1. It contains 12 lectures and 7 quizes";
+  const evs2 = "Environmental Science-2. It contains 12 lectures and 7 quizes";
+  const evsprac = "Environmental Science-Practical. It contains the practice of basic science experiments";
+
+  const cardData = [
+    {
+      title: "Environmental Science-1",
+      imgSrc: "/evs.png",
+      description: "12 lessons • 7 quiz",
+      instructor: {
+        name: "Aayush Talreja",
+        imgSrc: "https://media.licdn.com/dms/image/C4D03AQGjVJnZjakikg/profile-displayphoto-shrink_200_200/0/1657648501282?e=1685577600&v=beta&t=GZVLCtCI6mAVh0T4uTBJODcNWased_DRS3pxg73A7Vk",
+      },
+      link: "/videos",
+    },
+    {
+      title: "Environmental Science-2",
+      imgSrc: "/evs.png",
+      description: "12 lessons • 7 quiz",
+      instructor: {
+        name: "Niranjan Yeole",
+        imgSrc: "https://media.licdn.com/dms/image/C4E03AQFMaAxosx5O8A/profile-displayphoto-shrink_100_100/0/1642847850390?e=1685577600&v=beta&t=LDCn4WpFeU6vUMdAP6e9nQf4Cdz4DNs6zvy52GHkwVI",
+      },
+      link: "/videos",
+    },
+    {
+      title: "EVS-Practical",
+      imgSrc: "/evs.png",
+      description: "5 labs",
+      instructor: {
+        name: "Anuj Bagad",
+        imgSrc: "https://media.licdn.com/dms/image/C4D03AQG7ln3Kb53VZA/profile-displayphoto-shrink_100_100/0/1641273867979?e=1685577600&v=beta&t=QRlrRpePXwAuUynmY0zhCahF2ZU4cKns6uUqbDMdIZE",
+      },
+      link: "/videos",
+    },
+  ];
+
   return (
-    <div class=" m-3	">
+    <div className="m-3">
+      <div className="flex-wrap gap-5 pt-5 justify-content-center d-flex">
+        {cardData.map((card, index) => (
+          <div className="card" style={{ width: "18rem" }} key={index}>
+            <img src={card.imgSrc} className="card-img-top" alt="..." />
+            <div className="font-semibold text-gray-500 card-body bg-sky-200">
+              <h5 className="card-title fs-4">{card.title}</h5>
+              <button onMouseOver={() => speak({ text: card.description })}>
+                <img src={vol} alt="volume" />
+              </button>
+              <p className="p-2 card-text">{card.description}</p>
+              <p className="p-2 card-text">
+                <img className="w-10 rounded-full h-30" src={card.instructor.imgSrc} alt="" />
+                {card.instructor.name}
+              </p>
+              <Link to={card.link} className="justify-between btn btn-outline-primary">
+                Enroll
+              </Link>
 
-      {/* CARD IMAGE DIM.: 1163 x 1206 */}
-      <div className="d-flex flex-wrap gap-5 pt-5 ">
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="/evs.png" class="card-img-top" alt="..."/>
-          <div className="card-body text-gray-500 font-semibold bg-sky-200 ">
-            <h5 class="card-title fs-4">Environmental Science-1</h5>
-            <button onClick={() => speak({ text: evs })}>
-        <img src={vol} alt="volume"></img>
-        </button>
-            <p class="card-text p-2">
-            12 lessons • 7 quiz
-            </p>
-            <p class="card-text p-2">
-            <img className="h-30 w-10 rounded-full" src='https://media.licdn.com/dms/image/C4D03AQGjVJnZjakikg/profile-displayphoto-shrink_200_200/0/1657648501282?e=1685577600&v=beta&t=GZVLCtCI6mAVh0T4uTBJODcNWased_DRS3pxg73A7Vk' alt="" />
-            Aayush Talreja
-            </p>
-            <a href="videos" class="btn btn-outline-primary">
-            <Link to="/videos" class="nav-link active ">Enroll</Link>
-            </a>
+            </div>
           </div>
-        </div>
-
-        <div class="card" style={{ width: "18rem" }}>
-        <img src="/evs.png" class="card-img-top" alt="..."/>
-          <div className="card-body text-gray-500 font-semibold bg-sky-200 ">
-            <h5 class="card-title fs-4">Environmental Science-2</h5><button onClick={() => speak({ text: evs2 })}>
-        <img src={vol} alt="volume"></img>
-        </button>
-            <p class="card-text p-2">
-            12 lessons • 7 quiz
-            </p>
-            <p class="card-text p-2">
-            <img className="h-30 w-10 rounded-full" src='https://media.licdn.com/dms/image/C4E03AQFMaAxosx5O8A/profile-displayphoto-shrink_100_100/0/1642847850390?e=1685577600&v=beta&t=LDCn4WpFeU6vUMdAP6e9nQf4Cdz4DNs6zvy52GHkwVI' alt="" />
-            Niranjan Yeole
-            </p>
-            <a href="videos" class="btn btn-outline-primary">
-            <Link to="/videos" class="nav-link active">Enroll</Link>
-            </a>
-          </div>
-        </div>
-        <div class="card" style={{ width: "18rem" }}>
-        <img src="/evs.png" class="card-img-top" alt="..."/>
-          <div className="card-body text-gray-500 font-semibold bg-sky-200 ">
-            <h5 class="card-title fs-4">EVS-Practical</h5>
-            <button onClick={() => speak({ text: evsprac })}>
-              <img src={vol} alt="volume"></img>
-            </button>
-            <p class="card-text p-2">
-            5 labs
-            </p>
-            <p class="card-text p-2">
-            <img className="h-30 w-10 rounded-full" src='https://media.licdn.com/dms/image/C4D03AQG7ln3Kb53VZA/profile-displayphoto-shrink_100_100/0/1641273867979?e=1685577600&v=beta&t=QRlrRpePXwAuUynmY0zhCahF2ZU4cKns6uUqbDMdIZE' alt="" />
-            Anuj Bagad
-            </p>
-            <a href="videos" class="btn btn-outline-primary">
-            <Link to="/videos" class="nav-link active">Enroll</Link>
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
 }
+
 export default ScienceCatalogue;
--- src/MyComponents/SubjectCards.js
+++ src/MyComponents/SubjectCards.js
@@ -1,209 +1,84 @@
 import React from "react";
-import { useState, useRef } from "react";
-import math from "./../images/maths.png";
-import sci from "./../images/sci.png";
-import eng from "./../images/eng.png";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
-
-// function myChange(str) {
-//     var arr = str.split('');
-//     var result = "";
-//     for(var i=0; i < arr.length; i++) {
-//         if (arr[i] == 'o')  // if it's an o
-//             result += 'u';  // replace it with 'u'
-//         else
-//             result += arr[i];
-//     }
-// return(result);
-// }
-
-function braille() {
-  var br = require("braille");
-  var code = br.toBraille("this is in braille");
-  console.log(code);
-  toast("Braille \n" + code);
-}
+import braille from "braille";
+import sci from "./../images/sci.png";
+import math from "./../images/maths.png";
+import eng from "./../images/eng.png";
+import hin from "./../images/hin.png";
 
 export default function SubjectCards() {
-  // const [title, setTitle] = useState("Tanslate");
   const navigate = useNavigate();
-  const [buttonText, setButtonText] = useState("Translate To Braille");
 
-  function handleClick() {
-    var br = require("braille");
-    var code = br.toBraille("this line is in braille");
-    console.log(code);
-    // toast("Braille \n" + code);
-    setButtonText(code);
+  function handleClick(subject) {
+    const brailleText = braille.toBraille(`This is the ${subject} course in braille.`);
+    toast(`Braille: ${brailleText}`);
   }
+
+  const subjects = [
+    {
+      name: "Science",
+      description:
+        "Science helps us understand the world around us by exploring natural phenomena, conducting experiments, and making observations.",
+      imageSrc: sci,
+    },
+    {
+      name: "Mathematics",
+      description:
+        "Mathematics is the language of the universe, and it plays a vital role in various fields of science and technology.",
+      imageSrc: math,
+    },
+    {
+      name: "English",
+      description:
+        "English is a global language, and it's essential for communication, literature, and many other aspects of life.",
+      imageSrc: eng,
+    },
+    {
+      name: "Hindi",
+      description:
+        "Hindi, as one of India's official languages, plays a crucial role in fostering cultural identity and communication among its diverse population.",
+      imageSrc: hin,
+    },
+  ];
+
   return (
     <>
-      <div class="pt-6 pb-12 bg-gray-300">
-        <div id="card" class="mr-4">
-          {/* <h2 class="text-center uppercase text-4xl xl:text-5xl">
-            SUBJECTS
-          </h2> */}
-
-          <div class="container w-96 lg:w-4/5 mx-auto flex flex-col">
-            <div
-              v-for="card in cards"
-              class="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2"
-            >
-              <div class="h-64 w-auto md:w-1/2">
+      <Toaster />
+      <div className="pt-6 pb-12 bg-gray-300">
+        {subjects.map((subject) => (
+          <div key={subject.name} className="container flex flex-col mx-auto mt-4 w-96 lg:w-4/5">
+            <div className="flex flex-col mx-2 overflow-hidden bg-white rounded-lg shadow-xl hover:shadow-sm hover:cursor-pointer md:flex-row w-100">
+              <div className="w-auto h-64 md:w-1/2">
                 <img
-                  class="inset-0 h-full w-full object-cover object-center"
-                  src={sci}
+                  className="inset-0 object-cover object-center w-full h-full"
+                  src={subject.imageSrc}
+                  alt={`${subject.name} Image`}
                 />
               </div>
-
-              <div class="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
-                <h3 class="font-semibold  text-3xl self-center leading-tight uppercase truncate">
-                  Science
+              <div className="flex flex-col justify-between w-full px-6 py-4 text-gray-800">
+                <h3 className="self-center text-3xl font-semibold leading-tight uppercase truncate">
+                  {subject.name}
                 </h3>
-                {/* <h1 onClick={() => setTitle("New title")}>{title}</h1> */}
-                <p class="mt-1" id="myText">
-                  Science helps us understand the world around us by exploring
-                  natural phenomena, conducting experiments, and making
-                  observations.
-                </p>
-                {/* <p>
-                  <button
-                    className="btn btn-outline-secondary mt-2 mb-2"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseExample"
-                    aria-expanded="false"
-                    aria-controls="collapseExample"
-                    onClick={handleClick}
-                  >
-                    Translate In Braille
-                  </button>
-                </p>
-                <div class="collapse mt-2" id="collapseExample">
-                  <div class="card card-body ">
-                  {buttonText}
-                  </div>
-                </div> */}
-                {/* <button className="btn btn-outline  btn-secondary text-white" type="button" onClick={braille}>Translate To Braille</button> */}
-                <button class="w-[80%] lg:w-[30%]  inline-flex items-center justify-center p-0.5 mb-2  overflow-hidden text-sm font-medium text-[#212427] rounded-lg group bg-gradient-to-br from-[#A2DFD9] to-[#CB88FF] group-hover:from-[#A2DFD9] group-hover:to-[#CB88FF] hover:text-[#EFEEE9] focus:ring-2 focus:outline-none focus:ring-[#EFEEE9] " type="button" onClick={() => navigate("/science")}>
-                  <span class=" px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                <p className="mt-1">{subject.description}</p>
+                <button
+                  className="w-[80%] lg:w-[30%] inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-[#212427] rounded-lg group bg-gradient-to-br from-[#A2DFD9] to-[#CB88FF] group-hover:from-[#A2DFD9] group-hover:to-[#CB88FF] hover:text-[#EFEEE9] focus:ring-2 focus:outline-none focus:ring-[#EFEEE9]"
+                  type="button"
+                  onClick={() => navigate(`/${subject.name.toLowerCase()}`)}
+                >
+                  <span className="px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                     GO TO COURSE
                   </span>
                 </button>
-                {/* <button
-                  className="btn btn-outline-secondary self-start w-[80%] lg:w-[30%] mt-2 mb-2"
-                  type="button"
-                  onClick={() => navigate("/science")}
-                >
-                  Go To Course
-                </button> */}
-                <p class="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-1">
+                <p className="mt-1 text-sm font-semibold tracking-wide text-gray-700 uppercase">
                   REV-2019'C' Scheme
                 </p>
+                
               </div>
             </div>
           </div>
-
-          <div class="container w-96 lg:w-4/5 mx-auto flex flex-col">
-            <div
-              v-for="card in cards"
-              class="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2"
-            >
-              <div class="h-64 w-auto md:w-1/2">
-                <img
-                  class="inset-0 h-full w-full object-cover object-center"
-                  src={math}
-                />
-              </div>
-
-              <div class="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
-                <h3 class="font-semibold text-3xl self-center leading-tight uppercase truncate">
-                  Mathematics
-                </h3>
-                <p class="mt-1">
-                  Science helps us understand the world around us by exploring
-                  natural phenomena, conducting experiments, and making
-                  observations.
-                </p>
-                {/* <p>
-                  <button
-                    className="btn btn-outline-secondary mt-2 mb-2"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseExample"
-                    aria-expanded="false"
-                    aria-controls="collapseExample"
-                    onClick={handleClick}
-                  >
-                    Translate In Braille
-                  </button>
-                </p>
-                <div class="collapse mt-2" id="collapseExample">
-                  <div class="card card-body ">
-                  {buttonText}
-                  </div>
-                </div> */}
-                {/* <button
-                  className="btn btn-outline-secondary self-start w-[80%] lg:w-[30%] mt-2 mb-2"
-                  type="button"
-                  onClick={() => navigate("/math")}
-                >
-                  Go To Course
-                </button> */}
-                <button class="w-[80%] lg:w-[30%]  inline-flex items-center justify-center p-0.5 mb-2  overflow-hidden text-sm font-medium text-[#212427] rounded-lg group bg-gradient-to-br from-[#A2DFD9] to-[#CB88FF] group-hover:from-[#A2DFD9] group-hover:to-[#CB88FF] hover:text-[#EFEEE9] focus:ring-2 focus:outline-none focus:ring-[#EFEEE9] " type="button" onClick={() => navigate("/math")}>
-                  <span class=" px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    GO TO COURSE
-                  </span>
-                </button>
-                <p class="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-1">
-                  REV-2019'C' Scheme
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="container w-96 lg:w-4/5 mx-auto flex flex-col">
-            <div
-              v-for="card in cards"
-              class="flex flex-col md:flex-row overflow-hidden bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2"
-            >
-              <div class="h-64 w-auto md:w-1/2">
-                <img
-                  class="inset-0 h-full w-full object-cover object-center"
-                  src={eng}
-                />
-              </div>
-
-              <div class="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
-                <h3 class="font-semibold text-3xl self-center leading-tight uppercase truncate">
-                  English
-                </h3>
-                <p class="mt-1">
-                  Science helps us understand the world around us by exploring
-                  natural phenomena, conducting experiments, and making
-                  observations.
-                </p>
-                {/* <button
-                  className="btn btn-outline-secondary self-start w-[80%] lg:w-[30%] mt-2 mb-2"
-                  type="button"
-                  onClick={() => navigate("/english")}
-                >
-                  Go To Course
-                </button> */}
-                 <button class="w-[80%] lg:w-[30%]  inline-flex items-center justify-center p-0.5 mb-2  overflow-hidden text-sm font-medium text-[#212427] rounded-lg group bg-gradient-to-br from-[#A2DFD9] to-[#CB88FF] group-hover:from-[#A2DFD9] group-hover:to-[#CB88FF] hover:text-[#EFEEE9] focus:ring-2 focus:outline-none focus:ring-[#EFEEE9] " type="button" onClick={() => navigate("/english")}>
-                  <span class=" px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                    GO TO COURSE
-                  </span>
-                </button>
-                <p class="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-1">
-                  REV-2019'C' Scheme
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
